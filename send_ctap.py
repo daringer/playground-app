a8bf66a705fdaacab77a0c0a783ed9febdb648be
Markus Meissner <coder@safemailbox.de>
2022-09-26 19:16:31 +0200

    some minimal improvements for send_ctap.py

--- send_ctap.py
+++ send_ctap.py
@@ -5,11 +5,17 @@ devs = list(CtapHidDevice.list_devices())
 
 dev = devs[0]
 
+print("# import me:")
+print("> from send_ctap import dev")
+print("# and try:")
+print("> dev.call(0x75)")
 
-print(dev.call(0x75))
-
-
-print(dev.call(0x76))
+if __name__ == "__main__":
+    print(dev.call(0x75))
+    print(dev.call(0x76))
+    print(dev.call(0x77))
+    print(dev.call(0x78))
+    print(dev.call(0x79))
 
 
 
