9b2ffed7c8c52c0d7b1c3519638f13712d849216
Markus Meissner <coder@safemailbox.de>
2022-09-26 19:53:43 +0200

    send_ctap.py improved

--- send_ctap.py
+++ send_ctap.py
@@ -1,16 +1,16 @@
 from fido2.hid import CtapHidDevice
 
-
-devs = list(CtapHidDevice.list_devices())
-
-dev = devs[0]
+def get_dev():
+    devs = list(CtapHidDevice.list_devices())
+    return devs[0]
 
 print("# import me:")
-print("> from send_ctap import dev")
+print("> from send_ctap import get_dev")
 print("# and try:")
-print("> dev.call(0x75)")
+print("> get_dev().call(0x75)")
 
 if __name__ == "__main__":
+    dev = get_dev()
     print(dev.call(0x75))
     print(dev.call(0x76))
     print(dev.call(0x77))
