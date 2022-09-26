2e7dc736088ba44aba99e57dbfe201a14b1380c4
Markus Meissner <coder@safemailbox.de>
2022-09-26 19:54:06 +0200

    add api tests for ChangePin & SetClientContextPin

--- src/playground.rs
+++ src/playground.rs
@@ -9,7 +9,9 @@ use trussed::{
 
 const TEST1: VendorCommand = VendorCommand::H75;
 const TEST2: VendorCommand = VendorCommand::H76;
-
+const TEST3: VendorCommand = VendorCommand::H77;
+const TEST4: VendorCommand = VendorCommand::H78;
+const TEST5: VendorCommand = VendorCommand::H79;
 
 pub struct App<T>
 where T: TrussedClient,
@@ -33,23 +35,29 @@ where T: TrussedClient
         &[
             HidCommand::Vendor(TEST1),
             HidCommand::Vendor(TEST2),
+            HidCommand::Vendor(TEST3),
+            HidCommand::Vendor(TEST4),
+            HidCommand::Vendor(TEST5),
         ]
     }
 
     fn call(&mut self, command: HidCommand, input_data: &Message, response: &mut Message) -> hid::AppResult {
         match command {
             HidCommand::Vendor(TEST1) => {
-                debug_now!("test1");
+                debug_now!("test1 - write test data");
 
                 // write some dummy data
                 let data = Bytes::from_slice(b"abc-more-data.here").unwrap();
+                debug_now!("writing: {:?}", data);
                 syscall!(self.trussed.write_file(
                     Location::Internal,
                     PathBuf::from("test-filename"),
                     data,
                     None,
                 ));
-
+            }
+            HidCommand::Vendor(TEST2) => {
+                debug_now!("test2 - reading test data");
                 // read some dummy data
                 let result =
                     try_syscall!(self.trussed.read_file(Location::Internal, PathBuf::from("test-filename"),));
@@ -58,12 +66,35 @@ where T: TrussedClient
                     debug_now!("err loading: {:?}", result.err().unwrap());
                 } else {
                     let data = result.unwrap().data;
-                    debug_now!("data loaded: {:?}", data);
+                    debug_now!("reading: {:?}", data);
                 }
-
             }
-            HidCommand::Vendor(TEST2) => {
-                debug_now!("test2");
+            HidCommand::Vendor(TEST3) => {
+                debug_now!("test3 - setting pin in ClientContext to 123456");
+                let data = Bytes::from_slice(b"123456").unwrap();
+                syscall!(self.trussed.set_client_context_pin(data));
+            }
+            HidCommand::Vendor(TEST4) => {
+                debug_now!("test4 - change pin to 123456");
+                let data = Bytes::from_slice(b"123456").unwrap();
+                let res = try_syscall!(self.trussed.change_pin(data.clone()));
+
+                if res.is_err() {
+                    debug_now!("err changing pin: {:?}", res.err().unwrap());
+                } else {
+                    debug_now!("changed pin to: {:?}", data);
+                }
+            }
+            HidCommand::Vendor(TEST5) => {
+                debug_now!("test5 - change pin back to 1234");
+                let data = Bytes::from_slice(b"1234").unwrap();
+                let res = try_syscall!(self.trussed.change_pin(data.clone()));
+
+                if res.is_err() {
+                    debug_now!("err changing pin: {:?}", res.err().unwrap());
+                } else {
+                    debug_now!("changed pin to: {:?}", data);
+                }
             }
             _ => {
                 return Err(hid::Error::InvalidCommand);
