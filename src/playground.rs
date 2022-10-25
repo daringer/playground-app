c71743202cfee9d4069f243e9aa7ac4a647f50a4
Markus Meissner <coder@safemailbox.de>
2022-10-25 16:20:34 +0200

    test client for multi-backend, software authentication backend & pin handling

--- src/playground.rs
+++ src/playground.rs
@@ -1,9 +1,10 @@
 use ctaphid_dispatch::app::{self as hid, Command as HidCommand, Message};
 use ctaphid_dispatch::command::VendorCommand;
+use trussed::types::StorageAttributes;
 use trussed::{
     syscall, try_syscall, Bytes,
     Client as TrussedClient,
-    types::{PathBuf, Location, AuthContextID},
+    types::*,
 };
 
 
@@ -46,28 +47,34 @@ where T: TrussedClient
     fn call(&mut self, command: HidCommand, input_data: &Message, response: &mut Message) -> hid::AppResult {
         match command {
             HidCommand::Vendor(TEST1) => {
-                debug_now!("test1 - pin management");
+                debug_now!("############## test1 - pin management");
 
                 // admin pin checks
+                debug_now!("---------------------------");
                 debug_now!("------------- admin pin management tests:");
                 debug_now!("ADMIN - checking with '123456'");
                 let res = syscall!(self.trussed.check_auth_context(AuthContextID::Admin, b"123456"));
                 match res.authorized {
-                    true => debug_now!("ADMIN - check: success! retries left: {:?}", res.retries_left),
-                    false => debug_now!("ADMIN - check: fail! retries left: {:?}", res.retries_left),
+                    true => debug_now!("ADMIN - check: success!"),
+                    false => debug_now!("ADMIN - check: fail!"),
                 };
+
+                debug_now!("-> retries left: {:?}", syscall!(self.trussed.get_auth_retries_left(AuthContextID::Admin)));
 
                 debug_now!("ADMIN - checking with '1234'");
                 let res = syscall!(self.trussed.check_auth_context(AuthContextID::Admin, b"1234"));
                 match syscall!(self.trussed.check_auth_context(AuthContextID::Admin, b"1234")).authorized {
-                    true => debug_now!("ADMIN - check: success! retries left: {:?}", res.retries_left),
-                    false => debug_now!("ADMIN - check: fail! retries left: {:?}", res.retries_left),
+                    true => debug_now!("ADMIN - check: success!"),
+                    false => debug_now!("ADMIN - check: fail!"),
                 };
+
+                debug_now!("-> retries left: {:?}", syscall!(self.trussed.get_auth_retries_left(AuthContextID::Admin)));
+
 
                 debug_now!("ADMIN - set auth context to pin: 123456");
                 syscall!(self.trussed.set_auth_context(AuthContextID::Admin, b"123456"));
 
-                match try_syscall!(self.trussed.write_auth_context(AuthContextID::Admin, b"aaaa")) {
+                match try_syscall!(self.trussed.write_auth_context(b"aaaa")) {
                     Ok(_) => debug_now!("ADMIN - changed/wrote auth context state! (to 'aaaa')"),
                     Err(e) => debug_now!("ADMIN - error writing auth context state: {:?}", e),
                 };
@@ -75,35 +82,41 @@ where T: TrussedClient
                 debug_now!("ADMIN - checking ADMIN with '123456'");
                 let res = syscall!(self.trussed.check_auth_context(AuthContextID::Admin, b"123456"));
                 match res.authorized {
-                    true => debug_now!("ADMIN - check: success! retries left: {:?}", res.retries_left),
-                    false => debug_now!("ADMIN - check: fail! retries left: {:?}", res.retries_left),
+                    true => debug_now!("ADMIN - check: success!"),
+                    false => debug_now!("ADMIN - check: fail!"),
                 };
 
-                match try_syscall!(self.trussed.write_auth_context(AuthContextID::Admin, b"123456")) {
+                debug_now!("-> retries left: {:?}", syscall!(self.trussed.get_auth_retries_left(AuthContextID::Admin)));
+
+                match try_syscall!(self.trussed.write_auth_context(b"123456")) {
                     Ok(_) => debug_now!("ADMIN - changed/wrote auth context state! (to '123456')"),
                     Err(e) => debug_now!("ADMIN - error writing auth context state: {:?}", e),
                 };
 
                 // user pin checks
+                debug_now!("---------------------------");
                 debug_now!("------------- user pin management tests:");
                 debug_now!("USER - checking USER with '123456'");
                 let res = syscall!(self.trussed.check_auth_context(AuthContextID::User, b"123456"));
                 match res.authorized {
-                    true => debug_now!("USER - check: success! retries left: {:?}", res.retries_left),
-                    false => debug_now!("USER - check: fail! retries left: {:?}", res.retries_left),
+                    true => debug_now!("USER - check: success!"),
+                    false => debug_now!("USER - check: fail!"),
                 };
+
+                debug_now!("-> retries left: {:?}", syscall!(self.trussed.get_auth_retries_left(AuthContextID::User)));
 
                 debug_now!("USER - checking with '1234'");
                 let res = syscall!(self.trussed.check_auth_context(AuthContextID::User, b"1234"));
                 match res.authorized {
-                    true => debug_now!("USER - check: success! retries left: {:?}", res.retries_left),
-                    false => debug_now!("USER - check: fail! retries left: {:?}", res.retries_left),
+                    true => debug_now!("USER - check: success!"),
+                    false => debug_now!("USER - check: fail!"),
                 };
+                debug_now!("-> retries left: {:?}", syscall!(self.trussed.get_auth_retries_left(AuthContextID::User)));
 
                 debug_now!("USER - set auth context to pin: 1234");
                 syscall!(self.trussed.set_auth_context(AuthContextID::User, b"1234"));
 
-                match try_syscall!(self.trussed.write_auth_context(AuthContextID::User, b"aaaa")) {
+                match try_syscall!(self.trussed.write_auth_context(b"aaaa")) {
                     Ok(_) => debug_now!("USER - changed/wrote auth context state! (to 'aaaa')"),
                     Err(e) => debug_now!("USER - error writing auth context state: {:?}", e),
                 };
@@ -111,11 +124,12 @@ where T: TrussedClient
                 debug_now!("USER - checking with '1234'");
                 let res = syscall!(self.trussed.check_auth_context(AuthContextID::User, b"1234"));
                 match res.authorized {
-                    true => debug_now!("USER - check: success! retries left: {:?}", res.retries_left),
-                    false => debug_now!("USER - check: fail! retries left: {:?}", res.retries_left),
+                    true => debug_now!("USER - check: success!"),
+                    false => debug_now!("USER - check: fail!"),
                 };
+                debug_now!("-> retries left: {:?}", syscall!(self.trussed.get_auth_retries_left(AuthContextID::User)));
 
-                match try_syscall!(self.trussed.write_auth_context(AuthContextID::User, b"1234")) {
+                match try_syscall!(self.trussed.write_auth_context(b"1234")) {
                     Ok(_) => debug_now!("USER - changed/wrote auth context state! (to '1234')"),
                     Err(e) => debug_now!("USER - error writing auth context state: {:?}", e),
                 };
@@ -123,23 +137,58 @@ where T: TrussedClient
 
             }
             HidCommand::Vendor(TEST2) => {
-                debug_now!("test2 - reading test data");
-                // read some dummy data
-                /*let result =
-                    try_syscall!(self.trussed.read_file(Location::Internal, PathBuf::from("test-filename"),));
+                debug_now!("############## test2 - authenticated crypto operations");
 
-                if result.is_err() {
-                    debug_now!("err loading: {:?}", result.err().unwrap());
+                let mut creation_policy = Policy::new();
+                creation_policy.set_unauthorized(Permission::new().with_read(true));
+                creation_policy.set_user(Permission::new().with_read(true).with_encrypt(true).with_decrypt(true).with_sign(true).with_verify(true));
+                creation_policy.set_admin(Permission::new().with_all());
+
+                syscall!(self.trussed.set_creation_policy(creation_policy));
+
+                let res = try_syscall!(self.trussed.set_auth_context(AuthContextID::Admin, b"123456"));
+                if let Err(e) = res {
+                    debug_now!("{:?}", e);
                 } else {
-                    let data = result.unwrap().data;
-                    debug_now!("reading: {:?}", data);
-                }*/
+
+                    let s_attr = StorageAttributes::new().set_persistence(Location::Internal).set_serializable(true);
+                    let key_id = syscall!(self.trussed.generate_key(Mechanism::P256, s_attr)).key;
+
+                    debug_now!("key id: {:?}", key_id);
+
+                    let my_data = b"ospdfpokkpaasd";
+                    let res = try_syscall!(self.trussed.sign(Mechanism::P256, key_id, my_data, SignatureSerialization::Raw));
+                    if let Err(e) = res {
+                        debug_now!("{:?}", e);
+                    } else {
+                        debug_now!("{:?}", res.unwrap().signature);
+                    }
+
+
+                    let res = try_syscall!(self.trussed.set_auth_context(AuthContextID::Unauthorized, b"123456"));
+                    if let Err(e) = res {
+                        debug_now!("error setting auth context to unauth: {:?}", e);
+                    }
+
+
+                    let res = try_syscall!(self.trussed.sign(Mechanism::P256, key_id, my_data, SignatureSerialization::Raw));
+                    if let Err(e) = res {
+                        debug_now!("{:?}", e);
+                    } else {
+                        debug_now!("{:?}", res.unwrap().signature);
+                    }
+
+
+
+                }
             }
+
             HidCommand::Vendor(TEST3) => {
                 debug_now!("test3 - setting pin in ClientContext to 123456");
                 /*let data = Bytes::from_slice(b"123456").unwrap();
                 syscall!(self.trussed.set_client_context_pin(data));*/
             }
+
             HidCommand::Vendor(TEST4) => {
                 debug_now!("test4 - change pin to 123456");
                 /*let data = Bytes::from_slice(b"123456").unwrap();
@@ -151,6 +200,7 @@ where T: TrussedClient
                     debug_now!("changed pin to: {:?}", data);
                 }*/
             }
+
             HidCommand::Vendor(TEST5) => {
                 debug_now!("test5 - change pin back to 1234");
                 /*let data = Bytes::from_slice(b"1234").unwrap();
